04c0404f62e9f171c769dc3d70542e940b9949d3
Stanley <stanley.traub@gmail.com>
2020-09-28 14:22:40 -0700

    Initial Commit

---
+++ Fetch Rewards Coding Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch Rewards Coding Exercise
+//
+//  Created by Stanley Traub on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
